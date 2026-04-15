a151968eb0207b6a6fa22cba7bb926b9f0f32c33
silvaaaevelly <evelly.11762423@aluno.mg.gov.br>
2026-04-15 12:59:18 -0300

    Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+package pessoa;
+public class Passoa {
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+        public void caminhar () {
+            System.out.println("caminhando");
+        }
+        public void dormir (){
+            System.out.println ("dormir");
+        }
+    }
+    }
